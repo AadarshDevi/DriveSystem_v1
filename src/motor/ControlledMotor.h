27bfb5c3c58b59efe8b9fd6e1e9264b2641f060b
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-06-20 20:27:52 -0700

    class inherits from RawMotor

--- src/motor/ControlledMotor.h
+++ src/motor/ControlledMotor.h
@@ -5,7 +5,12 @@
 #ifndef DRIVESYSTEM_CONTROLLEDMOTOR_H
 #define DRIVESYSTEM_CONTROLLEDMOTOR_H
 
+#include "RawMotor.h"
+
 class ControlledMotor : public RawMotor {
+private:
+    max_power;
+
 public:
     ControlledMotor();
 };
