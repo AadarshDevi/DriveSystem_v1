34a4fe7ed41980b1e320d4a3597dd28bd2c78530
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-06-20 19:47:11 -0700

    PID Motor.cpp/h

---
+++ src/motor/ControlledMotor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by CryosArtic on 6/20/2026.
+//
+
+#include "ControlledMotor.h"
---
+++ src/motor/ControlledMotor.h
@@ -0,0 +1,13 @@
+//
+// Created by CryosArtic on 6/20/2026.
+//
+
+#ifndef DRIVESYSTEM_CONTROLLEDMOTOR_H
+#define DRIVESYSTEM_CONTROLLEDMOTOR_H
+
+// class ControlledMotor extends RawMotor {
+//     public:
+//     ControlledMotor();
+// }
+
+#endif //DRIVESYSTEM_CONTROLLEDMOTOR_H
