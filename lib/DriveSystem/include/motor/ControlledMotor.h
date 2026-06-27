5a0e0552efc619ef7c8a9b50c3e888ee7fa8ee42
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-06-27 13:51:30 -0700

    vars, constructor, methods headers created

--- lib/DriveSystem/include/motor/ControlledMotor.h
+++ lib/DriveSystem/include/motor/ControlledMotor.h
@@ -8,11 +8,45 @@
 #include "RawMotor.h"
 
 class ControlledMotor : public RawMotor {
-private:
-    // max_power;
+protected:
+    int minPower;
+    int maxPower;
+    int targetPower;
 
 public:
-    ControlledMotor();
+    ControlledMotor(
+        uint8_t new_pinPWM, uint8_t new_pinIN1, uint8_t new_pinIN2,
+        bool new_pinIN1Enabled, bool new_pinIN2Enabled,
+        bool new_reverseRotation, bool new_rotationEnabled,
+        int new_power, int new_absMinPower, int new_absMaxPower,
+        int new_minPower, int new_maxPower,
+        int new_shutdownPower, int new_targetPower,
+        bool new_showDebug
+    );
+
+    ControlledMotor(
+        uint8_t new_pinPWM, uint8_t new_pinIN1, uint8_t new_pinIN2,
+        int new_power, int new_absMinPower, int new_absMaxPower,
+        int new_minPower, int new_maxPower,
+        int new_shutdownPower, int new_targetPower
+    );
+
+    void setMinPower(int new_minPower);
+
+    void setMaxPower(int new_maxPower);
+
+    int getMinPower() const { return minPower; }
+
+    int getMaxPower() const { return maxPower; }
+
+    void setTargetPower(int new_targetPower);
+
+    int getTargetPower() const { return targetPower; }
+
+    /**
+     * PID controlling the motor's power to reach targetPower
+     */
+    void process();
 };
 
-#endif //DRIVESYSTEM_CONTROLLEDMOTOR_H
+#endif
